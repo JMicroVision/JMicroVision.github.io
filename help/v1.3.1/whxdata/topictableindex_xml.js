0b189ebcca650f095b8cc83b9eef1e67ab6d32da
Nicolas Roduit <nicolas.roduit@gmail.com>
2019-04-28 10:29:33 +0200

    Add help 1.3.1

---
+++ help/v1.3.1/whxdata/topictableindex_xml.js
@@ -0,0 +1,1 @@
+gXMLBuffer="<?xml version=\"1.0\" encoding=\"UTF-8\"?><cki><ck fm=\"Sending Bug Reports\" to=\"Histogram Thresholding\" num=\"61\"/></cki>";
---
+++ help/v1.3.1/whxdata/whidx_xml.js
@@ -0,0 +1,1 @@
+gXMLBuffer ="<?xml version='1.0' encoding='utf-8' ?>  <index>  <chunkinfo url=\"whidata0.xml\" first=\"1D Measurement\" last=\"Xor\" num=\"130\" />    </index>";
